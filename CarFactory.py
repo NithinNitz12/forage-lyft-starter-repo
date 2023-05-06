5079b9664d335c9d72d1b01ad2d712ea7a01dfdb
Nithin Nitz <nithinraj10@hotmail.com>
2023-05-06 18:47:00 +0530

    ♻️ Refactored

--- CarFactory.py
+++ CarFactory.py
@@ -1,41 +1,49 @@
-from battery import NubbinBattery
-from battery import SpindlerBattery
+from battery.NubbinBattery import NubbinBattery
+from battery.SpindlerBattery import SpindlerBattery
 
-from engine import CapuletEngine
-from engine import SternmanEngine
-from engine import WilloughbyEngine
+from engine.CapuletEngine import CapuletEngine
+from engine.SternmanEngine import SternmanEngine
+from engine.WilloughbyEngine import WilloughbyEngine
+
+from tyre.CarriganTyre import CarriganTyre
+from tyre.OctoprimeTyre import OctoprimeTyre
 
 from car import Car
 
 class CarFactory:
 
     @staticmethod
-    def CreateCalliope(CurrentDate, LastServiceDate, CurrentMileage, LastServiceMileage):
+    def CreateCalliope(CurrentDate, LastServiceDate, CurrentMileage, LastServiceMileage, SensorData):
         engine = CapuletEngine(CurrentMileage, LastServiceMileage)
         battery = SpindlerBattery(LastServiceDate, CurrentDate)
-        return Car(engine, battery)
+        tyre = CarriganTyre(SensorData)
+        return Car(engine, battery, tyre)
     
     @staticmethod
-    def CreateGlissade(CurrentDate, LastServiceDate, CurrentMileage, LastServiceMileage):
+    def CreateGlissade(CurrentDate, LastServiceDate, CurrentMileage, LastServiceMileage, SensorData):
         engine = WilloughbyEngine(CurrentMileage, LastServiceMileage)
         battery = SpindlerBattery(LastServiceDate, CurrentDate)
-        return Car(engine, battery)
+        tyre = CarriganTyre(SensorData)
+        return Car(engine, battery, tyre)
     
     @staticmethod
-    def CreatePalindrome(CurrentDate, LastServiceDate, WarningLightIsOn):
+    def CreatePalindrome(CurrentDate, LastServiceDate, WarningLightIsOn, SensorData):
         engine = SternmanEngine(WarningLightIsOn)
         battery = SpindlerBattery(LastServiceDate, CurrentDate)
-        return Car(engine, battery)
+        tyre = OctoprimeTyre(SensorData)
+        return Car(engine, battery, tyre)
     
     @staticmethod
-    def CreateRorschach(CurrentDate, LastServiceDate, CurrentMileage, LastServiceMileage):
+    def CreateRorschach(CurrentDate, LastServiceDate, CurrentMileage, LastServiceMileage, SensorData):
         engine = WilloughbyEngine(CurrentMileage, LastServiceMileage)
         battery = NubbinBattery(LastServiceDate, CurrentDate)
-        return Car(engine, battery)
+        tyre = OctoprimeTyre(SensorData)
+        return Car(engine, battery, tyre)
     
     @staticmethod
-    def CreateThovex(CurrentDate, LastServiceDate, CurrentMileage, LastServiceMileage):
+    def CreateThovex(CurrentDate, LastServiceDate, CurrentMileage, LastServiceMileage, SensorData):
         engine = CapuletEngine(CurrentMileage, LastServiceMileage)
         battery = NubbinBattery(LastServiceDate, CurrentDate)
-        return Car(engine, battery)
+        tyre = CarriganTyre(SensorData)
+        return Car(engine, battery, tyre)
     
--- car.py
+++ car.py
@@ -2,10 +2,11 @@ from Serviceable import Serviceable
 
 
 class Car(Serviceable):
-    def __init__(self, Engine, Battery):
+    def __init__(self, Engine, Battery, Tyre):
         self.__engine = Engine
         self.__battery = Battery
+        self.__tyre = Tyre
 
     
     def needs_service(self):
-        return self.__engine.NeedsService() or self.__battery.NeedsService()
+        return self.__engine.NeedsService() or self.__battery.NeedsService() or self.__tyre.NeedsService()
